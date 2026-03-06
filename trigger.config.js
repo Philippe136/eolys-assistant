05b2a278d22a1ec14df565a7c0495e86edf6d0db
philippe <saladinop136@gmail.com>
2026-03-06 13:33:25 +0100

    fix: set correct Trigger.dev project ref proj_ztpyizczroiwoigxlfom

--- trigger.config.js
+++ trigger.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from '@trigger.dev/sdk/v3';
 
 export default defineConfig({
-  project: 'eolys-assistant-edOi',
+  project: 'proj_ztpyizczroiwoigxlfom',
   runtime: 'node',
   logLevel: 'log',
   maxDuration: 300,
